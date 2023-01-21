c64a4406caf5317c8a95b9144a2528e9706aa4fb
filian <filiandun@gmail.com>
2023-01-21 13:29:08 +0300

    main: 100% ready

--- stack and overloading/main.cpp
+++ stack and overloading/main.cpp
@@ -9,41 +9,39 @@ int main()
     Stack stack_2(5, 101, 44, 12, 0, 988);
 
 
-// КОНСТРУКТОР КОПИРОВАНИЯ
-    Stack stack_2_copy = stack_2;
-    std::cout << "STACK 2: "; stack_2.print();
-    std::cout << "STACK 2 COPY: "; stack_2_copy.print(); std::cout << std::endl << std::endl;
-
-
 // COPY_IN
+    std::cout << "####ФУНКЦИЯ COPY_IN####" << std::endl << std::endl;
     Stack stack_1_copy;
 
-    std::cout << "STACK 1 BEFORE: "; stack_1.print();
-    std::cout << "STACK 1 COPY BEFORE: "; stack_1_copy.print();
+    std::cout << "STACK 1 ДО: "; stack_1.print();
+    std::cout << "STACK 1 ДО: "; stack_1_copy.print(); std::cout << std::endl;
 
-    std::cout << "АДРЕС: " << stack_1.copy_in(stack_1_copy) << std::endl;
-    std::cout << "STACK 1 AFTER: "; stack_1.print();
-    std::cout << "STACK 1 COPY AFTЕR: "; stack_1_copy.print(); std::cout << std::endl << std::endl;
+    std::cout << "АДРЕС СКОПИРОВАННОГО СТЕКА: " << stack_1.copy_in(stack_1_copy) << std::endl;
+
+    std::cout << "STACK 1 ПОСЛЕ: "; stack_1.print();
+    std::cout << "STACK 1 COPY ПОСЛЕ: "; stack_1_copy.print(); std::cout << std::endl << std::endl;
 
 
-// OPERATOR+ (РАБОТАЕТ)
+// OPERATOR+
+    std::cout << "####ФУНКЦИЯ OPERATOR+####" << std::endl << std::endl;
+
     std::cout << "STACK 1: "; stack_1.print();
     std::cout << "STACK 2: "; stack_2.print();
-    std::cout << "АДРЕС: " << stack_1 + stack_2 << std::endl << std::endl << std::endl;
+
+    std::cout << stack_1 + stack_2 << std::endl << std::endl << std::endl;
 
 
-// OPERATOR+ (РАБОТАЕТ)
-    Stack stack_3(7, 90, 1, 112, 21, 48, 90, 1001);
+// OPERATOR*
+    std::cout << "####ФУНКЦИЯ OPERATOR*####" << std::endl << std::endl;
+
+    Stack stack_3(7, 344, 1, 112, 21, 48, 90, 1001);
     Stack stack_4(6, 1001, 1, 76, 564, 19, 21);
 
-    //std::cout << "STACK 3: "; stack_3.print();
-    //std::cout << "STACK 4: "; stack_4.print();
+    std::cout << "STACK 3: "; stack_3.print();
+    std::cout << "STACK 4: "; stack_4.print();
 
-    //std::cout << "АДРЕС: " << stack_3 * stack_4 << std::endl;
+    std::cout << stack_3 * stack_4 << std::endl;
 
-
-    Stack stack_5 = (stack_3 * stack_4);
-    //stack_5.print();
 
 
     return 0;
